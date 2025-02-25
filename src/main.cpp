2ac68ded2020fb0ae3b9c1a6fc560c00c3ffcca9
Devs <85820587+Devs-leo@users.noreply.github.com>
2025-02-25 18:20:06 +0100

    Add main.cpp, tasks.txt and update Cmake The tasks.txt file is need to save on the disk the tasks create on the program. The updating of the Cmake file to include the tasks.txt file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 //
 // Created by Devs_leo on 19/02/2025.
 //
-#include <iostream>
+#include "Gui.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // Initialise the program
+    string filepath = "../../src/tasks.txt";  //Change here for a different disc file folder src, not cmake folder
+    vector<Task> tasks;
+    TodoList list(tasks, filepath);
+    int opMenu; // Variable that manage the value from the menuDisplay
+
+    //Start the main program
+    while(true){
+        opMenu = Gui::menuDisplay();
+        switch(opMenu){
+            case 1:
+                Gui::tasksDisplay(list);
+                break;
+            case 2:
+                Gui::addTaskDisplay(list);
+                break;
+            case 3:
+                Gui::completeTaskDisplay(list);
+                break;
+            case 4:
+                Gui::removeTaskDisplay(list);
+                break;
+            case 5:
+                Gui::exitProgram(list, filepath);
+                return 0;
+
+            default:
+                cout << "\nERROR opMenu: value is not well define.";
+                return 1;
+        }
+    }
 }
